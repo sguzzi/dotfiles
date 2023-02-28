0eb58f5c125555f6c5d317aa06b29d74c612c2d5
simone.guzzi <simone.guzzi@sib.swiss>
2023-02-28 13:21:09 +0100

    add aliases and nvm to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,17 @@
+f status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function nvm
+   bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
+
+alias ll 'ls -lahG'
+alias npm-reinstall 'rm -rf node_modules && rm -f package-lock.json && npm i --force'
+
+set -gx GPG_TTY (tty)
+
 starship init fish | source
